d05c06617c7fff7928623d592974f2ce14fd418d
201 escola <abiliomatute1@gmail.com>
2024-06-18 15:19:29 +0100

    identaçap

--- Classes/DungeonGenerate.cs
+++ Classes/DungeonGenerate.cs
@@ -7,11 +7,10 @@ public partial class DungeonGenerate : Node3D
 	[Export] public Node3D Rooms {get;set;}
 	[Export] public int Length {get; set;}
 	private int _roomsCount = 0;
-
-    public override void _Ready()
-    {
+    public override void _Ready(){
         
     }
+
     public void Generate(){
 	}
 }
