e1dd0ee4ed689f6504b73b670aaaef99894bf524
ftaplayz <abiliomatute1@gmail.com>
2024-07-04 01:20:24 +0100

    implementação da state machine

--- Classes/Enemy.cs
+++ Classes/Enemy.cs
@@ -4,26 +4,23 @@ using Godot;
 
 namespace GunRush.Classes;
 
-[Tool]
 public partial class Enemy : Humanoid
 {
-	[Export] public bool WalkTest { get; set; }
-	[Export] public bool ShootTest { get; set; }
+	private Player _enemy ;
 	public List<Node3D> Roams { get; set; } = new List<Node3D>();
 	private AnimationTree _animationTree;
-	private List<CollisionShape3D> _collisions = new List<CollisionShape3D>();
-	private bool _hasTarget = false;
+	public List<CollisionShape3D> Collisions = new List<CollisionShape3D>();
 	private Random _random = new Random();
+	private EnemyStateMachine _stateMachine;
 	
 	public override void _Ready()
 	{
+		this._stateMachine = new EnemyStateMachine(this);
 		this._animationTree = this.GetNode<AnimationTree>("AnimationTree");
 		this._GetCollisionShapes(this);
-	}
-
-	public override void _Process(double delta)
-	{
-		this._UpdateAnimationTree();
+		this._stateMachine.TransitionTo(this._stateMachine.Idle);
+		//this._stateMachine.TransitionTo(this._stateMachine.Shooting);
+		
 	}
 
 	private void _GetCollisionShapes(Node3D instance)
@@ -31,7 +28,7 @@ public partial class Enemy : Humanoid
 		foreach (var child in instance.GetChildren())
 		{
 			if(child is CollisionShape3D)
-				this._collisions.Add(child as CollisionShape3D);
+				this.Collisions.Add(child as CollisionShape3D);
 			if(child.GetChildCount() > 0)
 				this._GetCollisionShapes(child as Node3D);
 		}
@@ -42,26 +39,26 @@ public partial class Enemy : Humanoid
 		this._random = random;
 	}
 
-	private void _UpdateAnimationTree()
+	/*private void _UpdateAnimationTree()
 	{
 		var shooting = this._hasTarget || this.ShootTest;
 		var moving = false;
-		this._SetCondition("die", this.Health <= 0);
-		this._SetCondition("shoot", this._hasTarget || this.ShootTest);
+		this.SetCondition("die", this.Health <= 0);
+		this.SetCondition("shoot", this._hasTarget || this.ShootTest);
 		if(!this._hasTarget && !this.ShootTest){
 			moving = this.Velocity != Vector3.Zero || this.WalkTest;
-			this._SetCondition("walk", moving);
+			this.SetCondition("walk", moving);
 		}
-		this._SetCondition("idle", !shooting && !moving);
+		this.SetCondition("idle", !shooting && !moving);
 		
-	}
+	}*/
 
-	private void _SetCondition(string condition, bool value)
+	public void SetCondition(string condition, bool value)
 	{
 		this._animationTree.Set("parameters/conditions/" + condition, value);
 	}
 
-	private void _StartDeath()
+	/*private void _StartDeath()
 	{
 		foreach (var collision in this._collisions)
 			collision.Disabled = true;
@@ -71,15 +68,19 @@ public partial class Enemy : Humanoid
 	private void _EndDeath()
 	{
 		GD.Print("I am dead");
-	}
+	}*/
 	
 	private void _on_area_3d_damage_taken(double damage)
 	{
-		//GD.Print("And I sucked his cock in poland");
-		GD.Print(damage);
-		
 		this.Health -= (float)damage;
-		GD.Print(this.Health);
+		if(this.Health <= 0)
+			this._stateMachine.TransitionTo(this._stateMachine.Die);
+	}
+
+	public void Aggro(Player player)
+	{
+		this._enemy = player;
+		this._stateMachine.TransitionTo(this._stateMachine.Shooting);
 	}
 
 }
